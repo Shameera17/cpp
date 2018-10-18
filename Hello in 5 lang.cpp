07287e62e1a24d21409f50136265b31284fd64c8
Shameera Carrim <42635495+siara989@users.noreply.github.com>
2018-10-18 21:20:56 +0530

    Hello in  5 languages
    I made a source code to display hello in 5 languages.

---
+++ Hello in 5 lang.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main() {
+	using std::cout;
+	cout << "Display Hello in five languages\n";
+	cout << "1. English - Hello\n";
+	cout << "2. Sinhala - Ayubowan\n";
+	cout << "3. Malay - Salamat\n";
+	cout << "4. French - Bonjour\n";
+	cout << "5. Hindi - Namaste\n";
+	system("pause");
+	return 0;
+}
