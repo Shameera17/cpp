061e49034ed477b7d18502ddc6b15c4935da4326
Shameera Carrim <42635495+siara989@users.noreply.github.com>
2018-10-21 18:00:36 +0530

    formula for indices when using c++
    The formula used when using indices in c++ programming

---
+++ how to use the power in cpp/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+	using std::cout;
+	char base, power;
+	cout << "formula to be used when doing sums involved with indices\nS\n";
+	cout<<"pow(base, power)\n";
+	system("pause");
+	return 0;
+
+}
